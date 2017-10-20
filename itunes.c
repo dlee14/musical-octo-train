9455b22588c5523afb9b9bec0195e0332a555b93
jenyu7 <jyu7@stuy.edu>
2017-10-20 01:49:35 -0400

    added .h file

--- itunes.c
+++ itunes.c
@@ -3,27 +3,8 @@
 #include <ctype.h>
 #include <string.h>
 #include "head.h"
+#include "basic.c"
 
-//finds the index of a letter in the alphabet
-int index_of(char* s) {
-  char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-  
-  //finding the first occurence of the lowercase of the first character
-  char *ptr = strchr(alphabet, tolower(*s));
-
-  //find the address difference
-  int index = ptr - alphabet;
-  return index;
-}
-
-//prints a linked list
-void print_list(struct song* list) {
-  while (list) {
-    printf("%s: %s\n", list->artist, list->name);
-    list = list->next;
-  }
-  printf("NULL\n");
-}
 
 //inserts a song node at the front of a linked list
 struct song* insert_front(struct song* curr_list, struct song* song) {
