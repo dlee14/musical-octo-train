e51968bacf5ba1bf592339dc2d475a43188ad9dd
jenyu7 <jyu7@stuy.edu>
2017-10-22 16:18:09 -0400

    done

--- head.h
+++ head.h
@@ -15,8 +15,6 @@ struct song* song_at(struct song* list, int n);
 void print_artist(struct song* lib[], char* artist);
 void print_artist_help(struct song* list, char* artist);
 void print_letter(struct song* lib[], char* letter);
-void shuffle_list(struct song* list);
-void shuffle_lib(struct song* lib[]);
 
 //Processing functions
 
@@ -24,5 +22,9 @@ void insert_order(struct song* lib[], char* name, char* artist);
 struct song* insert_order_help(struct song* list, char* name, char* artist);
 struct song* create_node(struct song* next, char* name, char* artist);
 struct song* find_artist(struct song* lib[], char *artist);
-
-
+void shuffle_list(struct song* list);
+void shuffle_lib(struct song* lib[]);
+void remove_node(struct song* lib[], char* artist, char* name);
+struct song* remove_node_help(struct song* head, char* artist, char* name);
+void free_lib(struct song* lib[]);
+struct song* free_lib_help(struct song *ptr);
